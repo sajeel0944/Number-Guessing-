b32ff0e057a4a12c6197860e2946481451d57ff1
sajeek0944 <sajeel6y4z@gmail.com>
2024-03-26 15:55:05 +0500

    Game

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+console.log(chalk.yellowBright.bgGreenBright("\tWellcome to  number Guessing Game"));
+
+const RandomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessedNumber ",
+    type: "number",
+    message: chalk.magentaBright("\nguess a number in 1 to 6:\t"),
+  },
+]);
+if (answer.userGuessedNumber === RandomNumber) {
+  console.log(chalk.blueBright("\nyou guess right number."));
+} else {
+  console.log(chalk.redBright("\nyou guess wrong number"));
+}
